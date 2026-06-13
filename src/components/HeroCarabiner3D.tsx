f5964851ab34a367155c3c9d05c013268aad7177
samybit <samyb.samir@gmail.com>
2026-06-13 10:26:10 +0300

    feat(3d): add realistic tied coils along the straight section of 3 ropes

--- src/components/HeroCarabiner3D.tsx
+++ src/components/HeroCarabiner3D.tsx
@@ -501,14 +501,16 @@ function CarabinerModel() {
     pullRotation, 
     isYellow = false,
     knotType = 'clove',
-    removeFirstTail = false
+    removeFirstTail = false,
+    hasCoil = false
   }: { 
     position: THREE.Vector3, 
     barQuaternion: THREE.Quaternion, 
     pullRotation: [number, number, number], 
     isYellow?: boolean,
     knotType?: 'clove' | 'wrap' | 'messy',
-    removeFirstTail?: boolean
+    removeFirstTail?: boolean,
+    hasCoil?: boolean
   }) => {
     // We create a very long cylinder so it always stretches off-screen
     const ropeLength = 20;
@@ -552,6 +554,25 @@ function CarabinerModel() {
             <cylinderGeometry args={[0.08, 0.08, ropeLength, 16]} />
             {isYellow ? yellowRopeMaterial : silverRopeMaterial}
           </mesh>
+          
+          {/* Optional coiled/tied section on the straight rope */}
+          {hasCoil && (
+            <group position={[0, 0.8, 0]}>
+              {/* Stacked toruses to simulate a tightly wrapped coil/knot around the rope */}
+              <mesh rotation={[Math.PI / 2 + 0.1, 0, 0]} position={[0, -0.05, 0]}>
+                <torusGeometry args={[0.12, 0.07, 16, 32]} />
+                {isYellow ? yellowKnotMaterial : silverKnotMaterial}
+              </mesh>
+              <mesh rotation={[Math.PI / 2 - 0.1, 0, 0]} position={[0, 0.05, 0]}>
+                <torusGeometry args={[0.12, 0.07, 16, 32]} />
+                {isYellow ? yellowKnotMaterial : silverKnotMaterial}
+              </mesh>
+              <mesh rotation={[Math.PI / 2.5, 0, Math.PI / 4]} position={[0, 0, 0]}>
+                <torusGeometry args={[0.13, 0.05, 16, 32]} />
+                {isYellow ? yellowKnotMaterial : silverKnotMaterial}
+              </mesh>
+            </group>
+          )}
         </group>
       </group>
     );
@@ -595,15 +616,15 @@ function CarabinerModel() {
         {/* Because these are inside the group, they automatically pivot when the carabiner rotates! */}
         
         {/* Right Side Ropes */}
-        <Rope position={getPointOnLine(p1, p2, 0.26)} barQuaternion={side1.quaternion} pullRotation={[0, 0, -Math.PI / 3.5]} isYellow knotType="clove" />
+        <Rope position={getPointOnLine(p1, p2, 0.26)} barQuaternion={side1.quaternion} pullRotation={[0, 0, -Math.PI / 3.5]} isYellow knotType="clove" hasCoil />
         <Rope position={getPointOnLine(p1, p2, 0.8)} barQuaternion={side1.quaternion} pullRotation={[0, 0, -Math.PI / 1.7]} knotType="messy" />
 
         {/* Bottom Side Ropes */}
-        <Rope position={getPointOnLine(p2, p3, 0.2)} barQuaternion={side2.quaternion} pullRotation={[0, 0, -Math.PI * 0.85]} isYellow knotType="wrap" />
+        <Rope position={getPointOnLine(p2, p3, 0.2)} barQuaternion={side2.quaternion} pullRotation={[0, 0, -Math.PI * 0.85]} isYellow knotType="wrap" hasCoil />
         <Rope position={getPointOnLine(p2, p3, 0.75)} barQuaternion={side2.quaternion} pullRotation={[0, 0, Math.PI * 0.85]} knotType="clove" removeFirstTail />
 
         {/* Left Side Ropes */}
-        <Rope position={getPointOnLine(p3, p1, 0.35)} barQuaternion={side3.quaternion} pullRotation={[0, 0, Math.PI / 1.8]} knotType="messy" />
+        <Rope position={getPointOnLine(p3, p1, 0.35)} barQuaternion={side3.quaternion} pullRotation={[0, 0, Math.PI / 1.8]} knotType="messy" hasCoil />
         <Rope position={getPointOnLine(p3, p1, 0.8)} barQuaternion={side3.quaternion} pullRotation={[0, 0, Math.PI / 3.5]} isYellow knotType="wrap" />
 
       </group>
