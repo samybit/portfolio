5a75a9b5ac36821d46ea87686d52e1f3f2c731e1
samybit <samyb.samir@gmail.com>
2026-06-15 09:34:52 +0300

    seo: add google-site-verification metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://samyb.vercel.app'),
+  verification: {
+    google: "hV4rtyXu1OU2PEsZ2C9GnyTFakNpjmGILQvVWWV1Agc",
+  },
 };
 
 export default function RootLayout({
